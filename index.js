b800dadaa4fc2a3234c908b24dd31975960374d1
lealikaro9-source <lealikaro9@gmail.com>
2026-02-27 22:57:17 -0300

    index.js

--- index.js
+++ index.js
@@ -157,4 +157,4 @@ client.on('interactionCreate', async (interaction) => {
 });
 
 // FINAL: Sem aspas para pegar o valor real da variável
-client.login("MTQ3NjU3OTA1MTEyMzc2OTQ1Ng.GGqzV2.adPZhQzi0nIj3lvaGP29vjw5YKsPKG9bvnm8A4");
+client.login(TOKEN);
